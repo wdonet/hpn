688ff26420b262927037ce4cfca8bc5de94f59f1
Ozz <wdonet@gmail.com>
2016-09-08 09:57:36 -0700

    images and html with js

--- hpn.js
+++ hpn.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var hpn = express();
-
+hpn.set('port', (process.env.PORT || 8080));
+hpn.use(express.static(__dirname + '/public'));
+hpn.set('views', __dirname + '/views');
+hpn.set('view engine', 'ejs')
+;
 hpn.get('/', function (req, res) {
-    res.send("Hi")
+    //res.send("<h3>Hi</h3>");
+    res.render('index');
 });
 
-var port = 5000;
-hpn.listen(port, function() {
-    console.log('Listening on port ' + port);
+hpn.listen(hpn.get('port'), function() {
+    console.log('Listening on port ' + hpn.get('port'));
 });
