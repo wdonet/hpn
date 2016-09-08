9b1291bc73c01eae59458710a880056acc38ef5e
wdonet <wdonet@gmail.com>
2016-09-08 07:26:25 -0700

    first commit

---
+++ hpn.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var hpn = express();
+
+hpn.get('/', function (req, res) {
+    res.send("Hi")
+});
+
+var port = 8080;
+hpn.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
